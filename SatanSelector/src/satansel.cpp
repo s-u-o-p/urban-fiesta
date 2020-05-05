db180be9e721c8f98c96a19c99730dd420798cea
s-u-o-p <theguywhohasnoloaf@gmail.com>
2020-05-05 12:24:53 +1000

    fixed goingup thank god

--- SatanSelector/src/satansel.cpp
+++ SatanSelector/src/satansel.cpp
@@ -11,27 +11,29 @@ int peoplesub1 = -1 + people;
 string playername;
 
 void playerRegister(){
+int goingup = 1;
 
 while(0 != people){
-int goingup = 1;
-cout << "Player" << goingup << "'s Name - ";
+cout << "Player" << goingup << "Name - ";
 cin >> playername;
 cout << playername << " added! Only " << people - 1 << " left to go!\n";
 if(people <= 1){
     cout << "Everybody's in!\n";
 }
+cout << "\n";
+++goingup;
+--people;
 
 }
-
-
-
 
 }
 
 int main(){
 
-
-
-
+cout << "~~Satan Selector~~\n";
+cout << "How many people do you want to be in the running for Satan? - ";
+cin >> people;
+cout << "\n";
+playerRegister();
 
 }
