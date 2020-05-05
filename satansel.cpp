166c2c2512fb238a95d0a7356e1c465f63519959
s-u-o-p <theguywhohasnoloaf@gmail.com>
2020-05-05 10:35:38 +1000

    Creating Satan Selector

---
+++ satansel.cpp
@@ -0,0 +1,37 @@
+/*
+* SatanSelector
+* Author - suop
+*/
+
+#include <iostream>
+using namespace std;
+
+int people;
+int peoplesub1 = -1 + people;
+string playername;
+
+void playerRegister(){
+
+while(0 != people){
+int goingup = 1;
+cout << "Player" << goingup << "'s Name - ";
+cin >> playername;
+cout << playername << " added! Only " << people - 1 << " left to go!\n";
+if(people <= 1){
+    cout << "Everybody's in!\n";
+}
+
+}
+
+
+
+
+}
+
+int main(){
+
+
+
+
+
+}
