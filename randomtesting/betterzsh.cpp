8f773e932ab7b399bfc95407298022ffa3cd4a46
s-u-o-p <theguywhohasnoloaf@gmail.com>
2020-05-18 15:24:41 +1000

    betterzsh boiiiiiis

---
+++ randomtesting/betterzsh.cpp
@@ -0,0 +1,149 @@
+/*
+* made by suop, the better zsh
+*/
+
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+using namespace std;
+
+int no;
+bool isxorg = false;
+bool isyay = false;
+
+void Startx()
+{
+    system("kill -9 -1");
+}
+
+void Help()
+{
+    cout << "You can run: \n";
+    cout << "pacman    yes    sudo";
+    if (isxorg == true)
+    {
+        cout << "    startx";
+    }
+    if (isyay == true)
+    {
+        cout << "    yay";
+    }
+    cout << "\n";
+}
+
+void Pacman()
+{
+    string pacmaninput;
+    cout << "What package do you want to download?\n";
+    cout << "yay  ";
+    cout << "  xorg\n";
+    cin >> pacmaninput;
+    if (pacmaninput == "yay")
+    {
+        cout << "Installing Yay\n";
+
+        cout << "----------\n";
+        sleep(1);
+        cout << "0---------\n";
+        sleep(1);
+        cout << "00--------\n";
+        sleep(1);
+        cout << "000-------\n";
+        sleep(1);
+        cout << "0000------\n";
+        sleep(1);
+        cout << "00000-----\n";
+        sleep(1);
+        cout << "000000----\n";
+        sleep(1);
+        cout << "0000000---\n";
+        sleep(1);
+        cout << "00000000--\n";
+        sleep(1);
+        cout << "000000000-\n";
+        sleep(1);
+        cout << "0000000000\n";
+        cout << "Yay is now succesfully installed!\n";
+        isyay = true;
+    }
+    else if (pacmaninput == "xorg")
+    {
+        cout << "Installing xorg\n";
+
+        cout << "----------\n";
+        sleep(1);
+        cout << "0---------\n";
+        sleep(1);
+        cout << "00--------\n";
+        sleep(1);
+        cout << "000-------\n";
+        sleep(1);
+        cout << "0000------\n";
+        sleep(1);
+        cout << "00000-----\n";
+        sleep(1);
+        cout << "000000----\n";
+        sleep(1);
+        cout << "0000000---\n";
+        sleep(1);
+        cout << "00000000--\n";
+        sleep(1);
+        cout << "000000000-\n";
+        sleep(1);
+        cout << "0000000000\n";
+
+        cout << "Xorg is succesfully installed!\n";
+        isxorg = true;
+    }
+}
+
+void Yes()
+{
+    string yesinput;
+    cout << "input something:";
+    cin >> yesinput;
+    while (no != 1)
+    {
+        cout << yesinput;
+    }
+}
+
+void Sudo()
+{
+    string password;
+    cout << "[sudo] password for user: ";
+    cin >> password;
+    cout << "You are now not Superuser\n";
+}
+
+int main()
+{
+    string input;
+
+    while (no != 1)
+    {
+        cout << ">>> ";
+        cin >> input;
+
+        if (input == "yes")
+        {
+            Yes();
+        }
+        else if (input == "sudo")
+        {
+            Sudo();
+        }
+        else if (input == "pacman")
+        {
+            Pacman();
+        }
+        else if (input == "help")
+        {
+            Help();
+        }
+        else if (input == "startx" && isxorg == true)
+        {
+            Startx();
+        }
+    }
+}
---
+++ randomtesting/suopshell.cpp
@@ -0,0 +1,14 @@
+/*
+* made by suop
+* piss
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    string command;
+    cout << "piss > ";
+    cin >> command;
+    system(command);
+}
